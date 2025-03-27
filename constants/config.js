4ad4a2f255e8ffd8ae631b46332eb534f9b03832
Shivansh9848 <104058319+shivansh9848@users.noreply.github.com>
2025-03-27 22:12:51 +0530

    fix: remove localhost from CORS options to streamline configuration

--- constants/config.js
+++ constants/config.js
@@ -1,5 +1,5 @@
 const corsOptions = {
-  origin: ["http://localhost:5173/",process.env.CLIENT_URL],
+  origin: [process.env.CLIENT_URL],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
